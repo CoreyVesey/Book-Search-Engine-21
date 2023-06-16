ac91475f7cab6bc15ef41bc0944fea7557628a2f
Corey Vesey <vesey8507@gmail.com>
2023-06-15 18:36:48 -0700

    db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/BooksDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
