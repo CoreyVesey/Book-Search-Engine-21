540ef0b5c6fddd5d9ead5692840f68090159149d
Corey Vesey <vesey8507@gmail.com>
2023-06-18 12:23:31 -0700

    more typedef code

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,11 +2,41 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 type Book {
-
+    _id: Id
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
 }
 
 type User {
+    _id: Id
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+}
 
+type Auth {
+    user: User
+    token: Id
+}
+
+type Query {
+    usder: User
+}
+
+input: bookInput {
+    authors: String
+    title: String
+    description: String
+    bookId: String
+}
+
+type Mutation {
+    
 }
 `;
 
